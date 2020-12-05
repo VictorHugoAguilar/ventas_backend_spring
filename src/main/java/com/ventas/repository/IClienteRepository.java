729eac0561a87881613e0bbf2e3e7f1205a76145
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-12-05 13:27:48 +0100

    creacion de interface repository, creacion de clienteService

---
+++ src/main/java/com/ventas/repository/IClienteRepository.java
@@ -0,0 +1,7 @@
+package com.ventas.repository;
+
+import com.ventas.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+}
---
+++ src/main/java/com/ventas/service/IClienteService.java
@@ -0,0 +1,20 @@
+package com.ventas.service;
+
+import com.ventas.model.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IClienteService {
+
+    List<Cliente> findAll();
+
+    Optional<Cliente> findById(Integer id);
+
+    Cliente create(Cliente cliente);
+
+    Cliente update(Cliente cliente);
+
+    void delete(Integer id);
+
+}
